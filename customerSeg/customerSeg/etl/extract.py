57b01fa7942ead42feb6aff83a0753eac8502cf8
Jared Keil <55888596+jaredkeil@users.noreply.github.com>
2020-12-18 10:57:58 -0800

    handling str sources as filelike objs

--- customerSeg/customerSeg/etl/extract.py
+++ customerSeg/customerSeg/etl/extract.py
@@ -1,37 +1,78 @@
 from customerSeg.config import raw_dir
 from pathlib import Path
 import requests
-from pandas.io.common import is_url, is_file_like, 
-from pandas.io.parsers import 
+from io import StringIO
+from pandas.io.common import is_url, is_file_like, is_fsspec_url 
+from pandas.io.parsers import get_filepath_or_buffer
 
 # TODO
 # write methods for extracting from azure blob storage, aws s3, google cloud
 
 class Extract:
 
+    """
+    Extract data from storage.
+    """
+
     def __init__(self, source, target):
-        self.source = source
+        """
+        Accepted source formats: url, file-like (str or pathlib.Path), s3.
+        """
+        self.source = StringIO(str(source))
         self.target = target
-        self.fmt = self.check_source_format()
+        self.set_extract_method()
 
-    def check_source_format(self):
-        return 'url'
-
-    def from_url(self, sub_dir=''):
+    def _check_source_format(self, src):
+        src = self.source
+        if is_url(src):
+            fmt = 'url'
+        elif is_file_like(src):
+            fmt = 'filelike'
+        elif is_fsspec_url(src):
+            fmt = 's3'
+        else:
+            return 'invalid'
+        return fmt
+    
+    def set_extract_method(self):
+        input_type = self._check_source_format(self.source)
+        if input_type == 'url':
+            self.extract = self._extract_from_url
+        elif input_type == 'filelike':
+            self.extract = self._extract_from_filelike
+        elif input_type == 's3':
+            self.extract = self._extract_from_s3
+        
+    def _extract_from_url(self):
+        """Read from url"""
+        # Basic filename extraction. May fail if URL does not end with normal file extension
+        # GOOD  " ...com/data/file.csv "
+        # BAD   " ...com/data/file.csv?keyword=something " 
         url = self.source
+        filepath = self.target / Path(url).name
         try:
             response = requests.get(url, stream=True)
             response.raise_for_status()
         except requests.ConnectionError:
             print(f'Connection to {url} failed.')
             return None
-
-        # Basic filename extraction. May fail if URL does not end in extension
-        # GOOD  " ...com/data/file.csv "
-        # BAD   " ...com/data/file.csv?keyword=something " 
-        filepath = raw_dir / sub_dir / Path(url).name
         with open(filepath, 'wb') as f:
             f.writelines(response.iter_content(1024))
     
+    def _extract_from_filelike(self):
+        sourcepath = self.source
+        filepath = self.target / Path(sourcepath).name
+        try:
+            with open(sourcepath, 'rb') as src:
+                obj = src.read()
+                with open(filepath, 'wb') as f:
+                    f.write(obj)
+        except Exception as e:
+            print(e)
+            return None
 
+    def _extract_from_s3(self):
+        pass
 
+    def run(self):
+        self.extract()
