3e5246875cb099f1c73ef3ac8bf525f28e8c8fd3
Jared Keil <55888596+jaredkeil@users.noreply.github.com>
2021-01-03 09:44:08 -0800

    update conig locs

--- customerSeg/customerSeg/config.py
+++ customerSeg/customerSeg/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import customerSeg.utils
 
 # Should be root of 'customer-segmenter' repository
 root = Path(__file__).parents[2]
@@ -7,3 +8,7 @@ data_dir = root / 'data'
 raw_dir = data_dir / 'raw'
 interim_dir = data_dir / 'interim'
 merged_dir = interim_dir / 'merged'
+transformed_dir = data_dir / 'transformed'
+
+azure_connection_string = customerSeg.utils.get_env_var('AZURE_CONNECTION_STRING')
+container_name = "test"
