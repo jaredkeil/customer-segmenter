1947de386a7411a6164c757cb051778ed961b4e6
Jared Keil <55888596+jaredkeil@users.noreply.github.com>
2021-01-05 14:07:53 -0800

    update viz with filepaths, change var names

--- customerSeg/customerSeg/viz.py
+++ customerSeg/customerSeg/viz.py
@@ -4,7 +4,10 @@ import numpy as np
 from scipy import stats
 import pandas as pd
 
-from customerSeg.customers import customerGrouping
+from customerSeg.config import transformed_dir
+
+customer_data_path = transformed_dir
+customer_df = pd.read_csv()
 
 def analyze_skewness(x):
     """Visualize basic data transformations' effect on skewness of distribution.
@@ -14,14 +17,14 @@ def analyze_skewness(x):
         column name in customers, e.g. 'Recency', 'Frequency'
     """
     fig, ax = plt.subplots(2, 2, figsize=(5,5))
-    sns.distplot(customerGrouping[x], ax=ax[0,0])
-    sns.distplot(np.log(customerGrouping[x]), ax=ax[0,1])
-    sns.distplot(np.sqrt(customerGrouping[x]), ax=ax[1,0])
-    sns.distplot(stats.boxcox(customerGrouping[x])[0], ax=ax[1,1])
+    sns.distplot(customer_df[x], ax=ax[0,0])
+    sns.distplot(np.log(customer_df[x]), ax=ax[0,1])
+    sns.distplot(np.sqrt(customer_df[x]), ax=ax[1,0])
+    sns.distplot(stats.boxcox(customer_df[x])[0], ax=ax[1,1])
     plt.tight_layout()
     plt.show()
     
-    print(customerGrouping[x].skew().round(2))
-    print(np.log(customerGrouping[x]).skew().round(2))
-    print(np.sqrt(customerGrouping[x]).skew().round(2))
-    print(pd.Series(stats.boxcox(customerGrouping[x])[0]).skew().round(2))
+    print(customer_df[x].skew().round(2))
+    print(np.log(customer_df[x]).skew().round(2))
+    print(np.sqrt(customer_df[x]).skew().round(2))
+    print(pd.Series(stats.boxcox(customer_df[x])[0]).skew().round(2))
