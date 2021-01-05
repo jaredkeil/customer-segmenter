13492b1c994ef5c113dc7be70c51995a79e64cbf
Jared Keil <55888596+jaredkeil@users.noreply.github.com>
2021-01-05 14:05:42 -0800

    customerSeg/customerSeg/config.py

--- customerSeg/customerSeg/config.py
+++ customerSeg/customerSeg/config.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 import customerSeg.utils
 
-# Should be root of 'customer-segmenter' repository
+# Should be '~/path/to/customer-segmenter' repository
 root = Path(__file__).parents[2]
 
+### Directories
 data_dir = root / 'data'
 raw_dir = data_dir / 'raw'
+
 interim_dir = data_dir / 'interim'
+
 merged_dir = interim_dir / 'merged'
+merged_data_path = merged_dir / 'merge.csv'
+
 transformed_dir = data_dir / 'transformed'
+customer_table_path = transformed_dir / 'customer_table.csv'
 
 azure_connection_string = customerSeg.utils.get_env_var('AZURE_CONNECTION_STRING')
 container_name = "test"
