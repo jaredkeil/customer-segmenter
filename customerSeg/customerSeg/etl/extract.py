cfd8c59fc99540cc692aac864e0d169ce5680b87
Jared Keil <55888596+jaredkeil@users.noreply.github.com>
2021-01-07 11:03:24 -0800

    add example url path for Extract class to use in __main__

--- customerSeg/customerSeg/etl/extract.py
+++ customerSeg/customerSeg/etl/extract.py
@@ -1,12 +1,13 @@
-from customerSeg.config import raw_dir
 from pathlib import Path
 import requests
 from io import StringIO
 from pandas.io.common import is_url, is_file_like, is_fsspec_url 
 from pandas.io.parsers import get_filepath_or_buffer
+from customerSeg.config import raw_dir, interim_dir
 
 # TODO
 # write methods for extracting from azure blob storage, aws s3, google cloud
+# current s3 uri check is hacky/basic
 
 class Extract:
 
@@ -18,7 +19,8 @@ class Extract:
         """
         Accepted source formats: url, file-like (str or pathlib.Path), s3.
         """
-        self.source = StringIO(str(source))
+        # StringIO is expected type for imported pandas-based parser functions
+        self.source = StringIO(str(source))  
         self.target = target
         self._set_extract_method()
 
@@ -49,29 +51,32 @@ class Extract:
         return None
         
     def _extract_from_url(self):
-        """Read from url"""
-        # Basic filename extraction. May fail if URL does not end with normal file extension
-        # GOOD  " ...com/data/file.csv "
-        # BAD   " ...com/data/file.csv?keyword=something " 
+        """
+        Basic filename extraction. May fail if URL does not end with normal file extension. See examples.
+        GOOD : " ...com/data/file.csv ";  "...org/data/spreadsheet.xlsx"
+        BAD  : " ...com/data/file.csv?keyword=something "; "...com/data/file.csv?anything_after_csv"
+        """
         url = self.source
-        filepath = self.target / Path(url).name
+        # Set up destination savepath
+        savepath = self.target / Path(url).name
         try:
             response = requests.get(url, stream=True)
             response.raise_for_status()
         except requests.ConnectionError:
             print(f'Connection to {url} failed.')
+            # Exit early
             return None
-        with open(filepath, 'wb') as f:
-            f.writelines(response.iter_content(1024))
+        with open(savepath, 'wb') as sp:
+            sp.writelines(response.iter_content(1024))
     
     def _extract_from_filelike(self):
         sourcepath = self.source
-        filepath = self.target / Path(sourcepath).name
+        savepath = self.target / Path(sourcepath).name
         try:
             with open(sourcepath, 'rb') as src:
                 obj = src.read()
-                with open(filepath, 'wb') as f:
-                    f.write(obj)
+                with open(savepath, 'wb') as sp:
+                    sp.write(obj)
         except Exception as e:
             print(e)
             return None
@@ -81,3 +86,13 @@ class Extract:
 
     def extract(self):
         self._extract()
+
+
+if __name__ == '__main__':
+    # Path to example customer library
+    url_path = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx'
+    # save file to a pre-designated 'raw' data directory, as defined in customerSeg.config
+    extraction_path = raw_dir / 'Online_Retail.csv'
+
+    E = Extract(source=url_path, target=extraction_path)
+    E.extract()
